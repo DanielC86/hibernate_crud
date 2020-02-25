09125a10a2a46d317b2c2c1e9c68ebeb0b36845b
Daniel Czajka <daniel.czajka@gmail.com>
2020-02-25 17:06:20 +0100

    adding crud READ functionality

--- src/main/java/com/daniel/czajka/HibernateTutorialApplication.java
+++ src/main/java/com/daniel/czajka/HibernateTutorialApplication.java
@@ -26,14 +26,35 @@ public class HibernateTutorialApplication {
 
 			//create a student object
 			System.out.println("creating new user object");
-			Users newUser = new Users("monika", "monika@mail");
+			Users newUser = new Users("Mark", "mark@mail");
+
 			//start a transaction
 			session.beginTransaction();
 
 			//save the users object
 			session.save(newUser);
-			System.out.println("Saving new user");
+
+			System.out.println("Saving new user: " + newUser);
 			//commit transaction
+			session.getTransaction().commit();
+
+			//find out user id
+			System.out.println("Getting saved user ID (Primary key): " + newUser.getId());
+
+
+			//get new session
+			//start transaction
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+
+			//retrieve user based on ID
+			System.out.println("\n Getting saved user ID (Primary key): " + newUser.getId());
+
+			Users readUser = session.get(Users.class, newUser.getId());
+
+			System.out.println("\n Reading complete: " + readUser);
+
+			//commit the transaction
 			session.getTransaction().commit();
 
 			System.out.println("all done");
--- src/main/java/entity/Users.java
+++ src/main/java/entity/Users.java
@@ -1,9 +1,6 @@
 package entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="users")
@@ -12,6 +9,7 @@ public class Users {
 
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     private int id;
 
